33025fa1923edcc5871b411558f5666095dc9e62
iachieve <mhalim80@hotmail.com>
2020-10-28 08:45:56 -0700

    Bitwise AND &

--- src/dev/practice/one/And.java
+++ src/dev/practice/one/And.java
@@ -7,7 +7,10 @@ public class And {
 
         // 15b=> 0000 1111
         // 27b=> 0001 1011
-        //
+
+        //       0000 1011
+        //            8-21  // 1 2 4 8 16 32 64 128 .....etc
+        //            = 11
         // binary AND Operator (&)
         int and = a&b;
         System.out.println("AND Operator: " + and);
