e4ef8f0360d76ca6c1aa0afd1e2e3a7d8da3df04
iachieve <mhalim80@hotmail.com>
2020-10-28 09:01:32 -0700

    Bitwise Left Shift Operator <<

--- src/dev/practice/one/RightShift.java
+++ src/dev/practice/one/RightShift.java
@@ -1,15 +1,16 @@
 package dev.practice.one;
 
-public class LeftShift {
+public class RightShift {
     public static void main(String[] args) {
         int a = 15;
         int b = 27;
+
 
         // 15b=> 0000 1111
         //       0011 1100
         // 32+16+8+4 = 60
 
-        int leftShifted = a << 2;
-        System.out.println("right shift 2: " + leftShifted);
+        int rightShifted = a << 2;
+        System.out.println("right shift 2: " + rightShifted);
     }
 }
