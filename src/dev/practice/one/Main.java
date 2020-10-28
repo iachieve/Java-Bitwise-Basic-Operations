96b0966401c69be0891eff916fc1a97ecc6fc2c8
iachieve <mhalim80@hotmail.com>
2020-10-28 08:32:30 -0700

    initial commit

--- src/dev/practice/one/Main.java
+++ src/dev/practice/one/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("initial+commit");
-
+        System.out.println("initial+commit");
+        System.out.println("initial+commit");
+        System.out.println("initial+commit");
+        System.out.println("initial+commit");
     }
 }
