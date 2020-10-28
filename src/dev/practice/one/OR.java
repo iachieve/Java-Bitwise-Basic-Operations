d8dff6e1c507e81f9fba861acef024864c61a55e
iachieve <mhalim80@hotmail.com>
2020-10-28 08:47:52 -0700

    Bitwise OR |

--- src/dev/practice/one/OR.java
+++ src/dev/practice/one/OR.java
@@ -6,9 +6,10 @@ public class OR {
         int b = 27;
         // 15b=> 0000 1111
         // 27b=> 0001 1011
-
+        //       0001 1111
+        //          16+8+4+2+1 = 31
         // binary OR Operator (|)
         int or = a|b;
-        System.out.println("OR" + or);
+        System.out.println("OR: " + or);
     }
 }
