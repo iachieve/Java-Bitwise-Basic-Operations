7aa18ffd17d666b76469d2d4b715111e15aeb2cd
iachieve <mhalim80@hotmail.com>
2020-10-28 09:03:12 -0700

    Bitwise Right Shift Operator >>

--- src/dev/practice/one/RightShift.java
+++ src/dev/practice/one/RightShift.java
@@ -5,11 +5,9 @@ public class RightShift {
         int a = 15;
         int b = 27;
 
-
         // 15b=> 0000 1111
         //       0000 0011
         // 1 + 2 = 3
-
         int rightShifted = a >> 2;
         System.out.println("right shift 2: " + rightShifted);
     }
