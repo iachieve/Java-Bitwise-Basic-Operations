21c653d963064127a28e5676eb8e40f44147453e
iachieve <mhalim80@hotmail.com>
2020-10-28 08:51:09 -0700

    Bitwise XOR ^

--- src/dev/practice/one/XOR.java
+++ src/dev/practice/one/XOR.java
@@ -6,7 +6,8 @@ public class XOR {
         int b = 27;
         // 15b=> 0000 1111
         // 27b=> 0001 1011
-
+        //       0001 0100
+        //       16+4 = 20
         // binary XOR Operator (^)
         int xor = a^b;
         System.out.println("xor ^ :" + xor);
