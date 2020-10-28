abf43b647ecb97e0c05b0e91339224c58a08c999
iachieve <mhalim80@hotmail.com>
2020-10-28 08:54:52 -0700

    Bitwise ONCE_COMPLEMENT ~

--- src/dev/practice/one/OnceComplement.java
+++ src/dev/practice/one/OnceComplement.java
@@ -7,6 +7,8 @@ public class OnceComplement {
         // 15b=> 0000 1111
         // 27b=> 0001 1011
 
+        // ~X = (-X)-1 = -15-1 = -16
+        // (-) 0001 0000
         // binary One Complement Operator (~)
         int onceComplement = ~a;
         System.out.println("onceComplement ~ :" + onceComplement);
